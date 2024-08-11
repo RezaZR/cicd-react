48f3dc56a510027ac9e890d343bd2ca592e0cb06
RezaZR <rezazramadhan@gmail.com>
2024-08-11 22:01:05 +0700

    Chore: add commitlint and test

---
+++ commitlint.config.cjs
@@ -0,0 +1,21 @@
+const { plugins } = require("./.eslintrc.cjs");
+
+module.exports = {
+  extends: [],
+  rules: {
+    'header-min-length': [2, 'always', 10],
+    'header-case-start-capital': [2, 'always'],
+  },
+  plugins: [
+    {
+      rules: {
+        'header-case-start-capital': ({ raw }) => {
+          return [
+            /^[A-Z]/.test(raw),
+            'Commit message must start with a capital letter',
+          ];
+        },
+      },
+    },
+  ],
+};
